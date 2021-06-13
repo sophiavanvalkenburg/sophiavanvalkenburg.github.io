7c89cb8e22e05b00bbaca54e2e9bcddbd8a96070
sophiavanvalkenburg <sophiavanvalkenburg@users.noreply.github.com>
2021-06-12 23:04:05 -0400

    change ordering of projects

--- js/projects.js
+++ js/projects.js
@@ -43,6 +43,22 @@ const PROJECT_DATA = [
     previewImgType: 'png'
   },
   {
+    title: 'Ant World',
+    description: 'Concept illustration of a fantastical ant hill with 3D prototype of ant, created with three.js',
+    projectUrl: 'https://sophiaceleste.com/ant-world-3d/',
+    codeUrl: 'https://github.com/sophiavanvalkenburg/ant-world-3d',
+    previewImgUrl: 'previews/ant-world',
+    previewImgType: 'png'
+  },
+  {
+    title: 'Halloween Haus',
+    description: 'Many moons ago, 28 souls gathered at a haunted haus on all hallow\'s eve, unaware of the dark secrets lurking in their midst ... Based on true events! Do you join them on their day of spooky revelry? Play now, if you dare ...',
+    projectUrl: 'https://sophiaceleste.com/halloween-haus/',
+    codeUrl: 'https://github.com/sophiavanvalkenburg/halloween-haus/',
+    previewImgUrl: 'previews/halloween-haus',
+    previewImgType: 'gif'
+  },
+  {
     title: 'Canopy Cat Rescue',
     description: 'My Submission for Canopy Cat Rescue\'s T-Shirt Contest',
     projectUrl: 'https://sophiaceleste.com/canopy-cat/',
@@ -64,41 +80,11 @@ const PROJECT_DATA = [
     previewImgType: 'png'
   },
   {
-    title: 'Bug Mandala',
-    description: 'A psychedelic animated illustration of dancing bugs',
-    projectUrl: 'https://sophiaceleste.com/bug-mandala',
-    previewImgUrl: 'previews/bug-mandala',
-    previewImgType: 'gif'
-  },
-  {
-    title: 'Halloween Haus',
-    description: 'Many moons ago, 28 souls gathered at a haunted haus on all hallow\'s eve, unaware of the dark secrets lurking in their midst ... Based on true events! Do you join them on their day of spooky revelry? Play now, if you dare ...',
-    projectUrl: 'https://sophiaceleste.com/halloween-haus/',
-    codeUrl: 'https://github.com/sophiavanvalkenburg/halloween-haus/',
-    previewImgUrl: 'previews/halloween-haus',
-    previewImgType: 'gif'
-  },
-  {
     title: 'My Favorite Toy',
     description: 'A short graphic memoir',
     projectUrl: 'https://sophiaceleste.com/comics/my-favorite-toy/',
     previewImgUrl: 'comics/my-favorite-toy/img/my_favorite_toy_title_sm',
     previewImgType: 'png'
-  },
-  {
-    title: 'AR Builders',
-    description: 'Talk to CBRE Build\'s "Builders" in Augmented Reality! They\'ve lost some of their favorite items ... can you help? (Need to download & print the AR marker images in the code link below).',
-    projectUrl: 'https://floored.github.io/ar-builders',
-    codeUrl: 'https://github.com/sophiavanvalkenburg/ar-builders/',
-    previewImgUrl: 'previews/ar-builders-demo',
-    previewImgType: 'gif'
-  },
-  {
-    title: 'Rubiks Cube',
-    description: 'Model of a Rubiks Cube written in C++ and OpenGL from scratch.',
-    projectUrl: 'https://github.com/sophiavanvalkenburg/rubiks-cube/',
-    previewImgUrl: 'previews/rubiks-cube-demo',
-    previewImgType: 'gif'
   },
   {
     title: 'Eyeball Forest',
@@ -117,6 +103,28 @@ const PROJECT_DATA = [
     previewImgType: 'gif'
   },
   {
+    title: '"funkpeter (beta)" MV',
+    description: 'A music video for "funkpeter (beta)" by Psilodump',
+    projectUrl: 'https://vimeo.com/383631135',
+    previewImgUrl: 'previews/funkpeter-beta',
+    previewImgType: 'gif'
+  },
+  {
+    title: 'Bug Mandala',
+    description: 'A psychedelic animated illustration of dancing bugs',
+    projectUrl: 'https://sophiaceleste.com/bug-mandala',
+    previewImgUrl: 'previews/bug-mandala',
+    previewImgType: 'gif'
+  },
+  {
+    title: 'AR Builders',
+    description: 'Talk to CBRE Build\'s "Builders" in Augmented Reality! They\'ve lost some of their favorite items ... can you help? (Need to download & print the AR marker images in the code link below).',
+    projectUrl: 'https://floored.github.io/ar-builders',
+    codeUrl: 'https://github.com/sophiavanvalkenburg/ar-builders/',
+    previewImgUrl: 'previews/ar-builders-demo',
+    previewImgType: 'gif'
+  },
+  {
     title: 'The Wrigglies',
     description: 'ITP Camp 2017 Project<br>music credit: Psilodump',
     projectUrl: 'https://sophiaceleste.com/wrigglies-itp-2017',
@@ -125,18 +133,10 @@ const PROJECT_DATA = [
     previewImgType: 'gif'
   },
   {
-    title: '"funkpeter (beta)" MV',
-    description: 'A music video for "funkpeter (beta)" by Psilodump',
-    projectUrl: 'https://vimeo.com/383631135',
-    previewImgUrl: 'previews/funkpeter-beta',
+    title: 'Rubiks Cube',
+    description: 'Model of a Rubiks Cube written in C++ and OpenGL from scratch.',
+    projectUrl: 'https://github.com/sophiavanvalkenburg/rubiks-cube/',
+    previewImgUrl: 'previews/rubiks-cube-demo',
     previewImgType: 'gif'
-  },
-  {
-    title: 'Ant World',
-    description: 'Concept illustration of a fantastical ant hill with 3D prototype of ant, created with three.js',
-    projectUrl: 'https://sophiaceleste.com/ant-world-3d/',
-    codeUrl: 'https://github.com/sophiavanvalkenburg/ant-world-3d',
-    previewImgUrl: 'previews/ant-world',
-    previewImgType: 'png'
   }
 ];
