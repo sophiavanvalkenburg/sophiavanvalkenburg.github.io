585ad3f90f2865965cdae2cba55d1898b425527d
sophiavanvalkenburg <sophiavanvalkenburg@users.noreply.github.com>
2021-06-14 19:44:59 -0400

    add contact skeleton

--- js/header.js
+++ js/header.js
@@ -3,5 +3,5 @@ navbar.className = 'navbar sticky-top navbar-light';
 navbar.innerHTML = '<a class="navbar-brand">SOPHIA CELESTE</a>';
 navbar.innerHTML += '<a class="nav-link nav-work-link" href="/">WORK</a>';
 navbar.innerHTML += '<a class="nav-link nav-about-link" href="/about">ABOUT</a>';
-navbar.innerHTML += '<a class="nav-link" href="/contact">CONTACT</a>';
+navbar.innerHTML += '<a class="nav-link nav-contact-link" href="/contact">CONTACT</a>';
 document.body.appendChild(navbar);
