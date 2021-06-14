5b1ebab0e02e726ab23445817e684403ac7e476a
sophiavanvalkenburg <sophiavanvalkenburg@users.noreply.github.com>
2021-06-14 19:35:50 -0400

    move header and footer to js files

---
+++ js/footer.js
@@ -0,0 +1,8 @@
+let footer = document.createElement('footer');
+footer.innerHTML = '<a class="social-media-link" href="https://www.instagram.com/sophiaceleste.art" target="_blank">';
+footer.innerHTML += '<img src="/img/instagram-logo.png"></a>';
+footer.innerHTML +=  '<a class="social-media-link" href="https://github.com/sophiavanvalkenburg/" target="_blank">';
+footer.innerHTML += '<img src="/img/github-logo.png"/></a>'; 
+footer.innerHTML += '<a class="social-media-link" href="https://www.linkedin.com/in/sophiavv/" target="_blank">';
+footer.innerHTML += '<img src="/img/linkedin-logo.png"/></a>';
+document.body.appendChild(footer);  
---
+++ js/header.js
@@ -0,0 +1,7 @@
+let navbar = document.createElement('nav');
+navbar.className = 'navbar sticky-top navbar-light';
+navbar.innerHTML = '<a class="navbar-brand">SOPHIA CELESTE</a>';
+navbar.innerHTML += '<a class="nav-link nav-work-link" href="/">WORK</a>';
+navbar.innerHTML += '<a class="nav-link nav-about-link" href="/about">ABOUT</a>';
+navbar.innerHTML += '<a class="nav-link" href="/contact">CONTACT</a>';
+document.body.appendChild(navbar);
