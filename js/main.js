759746dee84dee847e90fdf7c0db259c946828c7
sophiavanvalkenburg <sophiavanvalkenburg@users.noreply.github.com>
2021-06-14 10:09:50 -0400

    change design of navbar and project cards

--- js/main.js
+++ js/main.js
@@ -6,21 +6,11 @@ for ( let i = 0; i < PROJECT_DATA.length; i++ ){
 
 function generateProjectCard(project) {
   let cardHtml = '' +
-        '<div class="project-card card">' +
-        '<div class="card-body">' +
+        '<div class="preview-img-container">' +
               '<div class="preview-img">';
-    if (project.previewImgType === 'gif') {
-      cardHtml += '<img class="preview-img-gif" src="' + project.previewImgUrl + '.gif" />';
-    }
-      cardHtml += '<img class="preview-img-png ' + (project.previewImgType !== 'gif' ? 'no-gif': '') + '" src="' + project.previewImgUrl + '.png" />' +
-              '</div>' +
-              '<h5 class="project-card-title">' + project.title + '</h5>' +
-                '<p>' + project.description + '</p>';
-      cardHtml += '<a class="stretched-link" href="' + project.projectUrl + '" target="_blank"</a>' +
-      '</div>';
-      if (typeof(project.codeUrl) !== 'undefined') {
-        cardHtml += '<a class="code-link" href="' + project.codeUrl + '" target="_blank">View Code on GitHub</a></div>';
-      }
-      cardHtml += '</div>';
+    cardHtml += '<a href="' + project.projectUrl + '">';
+    cardHtml += '<img class="preview-img-png ' + (project.previewImgType !== 'gif' ? 'no-gif': '') + '" src="' + project.previewImgUrl + '.png" />'
+    cardHtml += '<div class="img-overlay"><h5 class="project-title">' + project.title + '</h5></div>'
+    cardHtml += '</a></div></div>';
   return cardHtml;
 }
