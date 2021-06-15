cd6f612bac3875853544ca4529182644b552dc94
sophiavanvalkenburg <sophiavanvalkenburg@users.noreply.github.com>
2021-06-15 12:27:00 -0400

    get started with eleventy

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('img');
+    eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPassthroughCopy('js');
+    eleventyConfig.addPassthroughCopy('lib')
+    return {
+        passthroughFileCopy: true
+    };
+};
--- js/footer.js
+++
@@ -1,5 +0,0 @@
-let footer = document.createElement('footer');
-footer.innerHTML = '<a class="social-media-link" href="https://www.instagram.com/sophiaceleste.art" target="_blank"><img src="/img/instagram-logo.png"/></a>';
-footer.innerHTML +=  '<a class="social-media-link" href="https://github.com/sophiavanvalkenburg/" target="_blank"><img src="/img/github-logo.png"/></a>'; 
-footer.innerHTML += '<a class="social-media-link" href="https://www.linkedin.com/in/sophiavv/" target="_blank"><img src="/img/linkedin-logo.png"/></a>';
-document.body.appendChild(footer);  
