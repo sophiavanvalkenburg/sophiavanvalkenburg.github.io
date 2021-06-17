3d7a6d35d4756be9fb3eb697fbc5a6477302832f
sophiavanvalkenburg <sophiavanvalkenburg@users.noreply.github.com>
2021-06-16 23:28:20 -0400

    fix gallery view, add gallery items, fix some single image previews

---
+++ js/masonry-init.js
@@ -0,0 +1,14 @@
+// init Masonry
+var grid = document.querySelector('.masonry-grid');
+
+var msnry = new Masonry( grid, {
+  itemSelector: '.masonry-grid-item',
+  columnWidth: '.masonry-grid-sizer',
+  percentPosition: true,
+  transitionDuration: 0
+});
+
+imagesLoaded( grid ).on( 'progress', function() {
+  // layout Masonry after each image loads
+  msnry.layout();
+});
